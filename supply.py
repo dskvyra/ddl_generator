af6ac150b95d8ca09efe4785d831693a8237eeb8
Dmitry Skvira <flyerok@gmail.com>
2014-02-24 11:36:06 +0200

    Update syntax required sql-syntax in supply.py

--- supply.py
+++ supply.py
@@ -12,18 +12,15 @@
 
 	print "Hello"
 
-	create sequence autoincrement_article_id;
-	create table article (
-	article_id int primary key default nextval('autoincrement_article_id'),
-	article_title varchar(50),
-	article_text text
+	CREATE TABLE "category" (
+	category_id serial,
+	category_title varchar(50) NOT NULL,
+	PRIMARY KEY "category_id"
 	);
 
-	create sequence autoincrement_category_id;
-	create table category (
-	category_id int primary key default nextval('autoincrement_category_id'),
-	category_title varchar(50)
+	CREATE TABLE "article" (
+	article_id serial,
+	article_text text NOT NULL,
+	article_title varchar(50) NOT NULL,
+	PRIMARY KEY "article_id"
 	);
-
-
-	article (с полями article_id, article_title, article_text) и category (category_id, category_title). 
