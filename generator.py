f471d634f5d7ee30a5063a9ee129474b1ca5b6cc
Flyer <flyer@gmail.com>
2014-03-29 13:15:43 +0200

    Add update function & trigger

--- generator.py
+++ generator.py
@@ -13,10 +13,20 @@ class DDLStatement(object):
 		for key in self.fields:
 			field_string = '{table_name}_{field_name} {field_type},'
 			entity.append(field_string.format(table_name = self.table_name, field_name = key, field_type = self.fields[key]))
+		update_function = """CREATE OR REPLACE FUNCTION {0}_on_update() RETURNS trigger AS ${0}_on_update$
+	BEGIN
+		NEW.{0}_updated := current_timestamp;
+		RETURN NEW;
+	END;
+${0}_on_update$ LANGUAGE plpgsql;""".format(self.table_name) 
+
+		update_trigger = """CREATE TRIGGER {0}_update_trigger BEFORE UPDATE ON category
+FOR EACH ROW
+EXECUTE PROCEDURE {0}_on_update();""".format(self.table_name) 
 		entity.extend(['%s_created timestamp NOT NULL DEFAULT current_timestamp,' % self.table_name,
 						'%s_updated timestamp NOT NULL DEFAULT 0,' % self.table_name,
 						'PRIMARY KEY (%s_id)' % self.table_name,
-						');\n'])
+						');', update_function, update_trigger])
 		return '\n'.join(entity)
 
 def generate_statement(from_yaml):
